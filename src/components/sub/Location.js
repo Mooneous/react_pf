1ebfc29e21ed143a2087da29b3a9880c14b33457
Mooneous <4087022@naver.com>
2022-08-11 14:05:59 +0900

    버튼 활성화 방법2

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -31,7 +31,7 @@ function Location() {
 
 	const container = useRef(null);
 	//버튼 가상돔요소를 참조할 빈 객체 생성
-	const btns = useRef(null);
+	//const btns = useRef(null);
 	const [Location, setLocation] = useState(null);
 	const [Traffic, setTraffic] = useState(false);
 	//해당 지도관련 정보값이 변경될때마다 화면을 다시 렌더링하고 return문에서 편하게 호출하기 위해 Info 스테이트에 옮겨담음
@@ -70,8 +70,8 @@ function Location() {
 		setLocation(map_instance);
 
 		//버튼활성화
-		for (const btn of btns.current.children) btn.classList.remove('on');
-		btns.current.children[Index].classList.add('on');
+		// 	for (const btn of btns.current.children) btn.classList.remove('on');
+		// 	btns.current.children[Index].classList.add('on');
 	}, [Index]);
 	//▲기존에 의존성 배열이 비어져 있을때에는 그냥 컴포넌트 마운트시 지도가 한번 호출되고 마는 구조
 	//의존성 배열에 Index스테이트를 등록하면 지점버튼을 클릭해서 Index스테이트가 변경될때마다 새로 변경된 Index값 정보를 토대로 지도 재호출
@@ -94,13 +94,16 @@ function Location() {
 			<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic OFF' : 'Traffic ON'}</button>
 
 			{/* 지점보기 버튼 추가후 버튼 클릭시 해당 버튼의 순서값으로 Index스테이트 변경 */}
-			<ul className='branch' ref={btns}>
+			{/* <ul className='branch' ref={btns}> */}
+			<ul className='branch'>
 				{/* <li onClick={() => setIndex(0)}>궁남지</li>
 				<li onClick={() => setIndex(1)}>올림픽 공원</li>
 				<li onClick={() => setIndex(2)}>서울 시청</li> */}
-				{Info.map((info, idx) => {
+				{info.map((info, idx) => {
+					let on = '';
+					Index === idx && (on = 'on');
 					return (
-						<li key={idx} onClick={() => setIndex(idx)}>
+						<li key={idx} onClick={() => setIndex(idx)} className={on}>
 							{info.title}
 						</li>
 					);
