ac2795b18b840c765da6762d116dcfbaf9a845ec
Mooneous <4087022@naver.com>
2022-08-16 13:23:53 +0900

    메인컴포넌트에서 데이터를 전달 받기

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,12 +1,27 @@
 import { useState, useEffect } from 'react';
 
 function News() {
-	const [Posts, setPosts] = useState([]);
+	//로컬스토리지에 있는 데이터를 가져와서 다시 JSON객체로 parsing해서 리턴하는 함수
+	const getLocalData = () => {
+		const dummyPosts = [
+			{ title: 'Hello5', content: 'Here comes description in detail.' },
+			{ title: 'Hello4', content: 'Here comes description in detail.' },
+			{ title: 'Hello3', content: 'Here comes description in detail.' },
+			{ title: 'Hello2', content: 'Here comes description in detail.' },
+			{ title: 'Hello1', content: 'Here comes description in detail.' },
+		];
+		const data = localStorage.getItem('post');
+		if (data) {
+			return JSON.parse(data);
+		} else {
+			return dummyPosts;
+		}
+	};
+
+	const [Posts] = useState(getLocalData());
 
 	useEffect(() => {
-		let data = localStorage.getItem('post');
-		data = JSON.parse(data);
-		setPosts(data);
+		localStorage.setItem('post', JSON.stringify(Posts));
 	}, []);
 
 	return (
