2b89bb44b53f7c415598c60f3126d44ffce6fe6c
Mooneous <4087022@naver.com>
2022-08-16 17:04:20 +0900

    브라우저 리사이즈시 해당 섹션위치값을 useRef로 생성한 참조객체에 저장

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,10 +7,26 @@ import Vids from './Vids';
 import { useRef, useEffect } from 'react';
 
 function Main() {
+	//브라우저 리사이즈시 메인 컴포넌트 호출이 되는지안되는지 알아보려고~
+	console.log('main called'); //처음에만 호출되고 리사이즈시 호출안됨
+
 	const main = useRef(null);
+	const pos = useRef([]);
+
+	const getPos = () => {
+		pos.current = []; //리사이즈시마다 위치값(pos) 추가되고있으므로 빈배열 만들어 초기화한 다음 담기
+		const secs = main.current.querySelectorAll('.myScroll');
+		//반복문(메인페이지섹션높이값)
+		for (const sec of secs) pos.current.push(sec.offsetTop);
+		console.log(pos.current);
+	};
 
 	useEffect(() => {
-		const secs = main.current.querySelectorAll('.myScroll');
+		getPos();
+		window.addEventListener('resize', getPos);
+		return () => {
+			window.removeEventListener('resize', getPos);
+		};
 	}, []);
 
 	return (
@@ -25,3 +41,12 @@ function Main() {
 }
 
 export default Main;
+
+/*
+	useRef : state와는 달리 useRef에 담겨있는 값은 변경이 되도 화면이 재랜더링되지 않으면서
+	만약 다른 state변경으로 화면이 재랜더링 되었을때 일반 변수값처럼 useRef에 담겨있는 값은 사라지지 않음
+vs.
+  state : 해당값이 변경이 되면 무조건 해당 컴포넌트는 재랜더링 일어남 (화면의 변경점을 담당하는 화면UI에 관련 데이터)
+	지역변수 : 컴포넌트가 재랜더링될떄마다 해당 변수에 있는 값은 유지가 안되고 다시 초기화 (권장사항 아님)
+	useRef : 컴포넌트가 재랜더링되더라도 값이 유지가됨, 해당 값이 변경되어도 화면을 재랜더링하지 않음 (화면UI의 직접적인 변경사항은 아니나 특정 정보값을 단기간에 엄청 많이 변경해야될때)
+*/
--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,30 +1,7 @@
-import Anime from '../../assets/Anime';
-import { useRef } from 'react';
-
-const btnStyle = {
-	position: 'absolute',
-	top: 120,
-	left: 100,
-};
-
 function Visual() {
 	const box = useRef(null);
 
-	return (
-		<figure id='visual' className='myScroll'>
-			<button
-				style={btnStyle}
-				onClick={() => {
-					new Anime(window, {
-						prop: 'scroll',
-						value: 600,
-						duration: 500,
-					});
-				}}>
-				start
-			</button>
-		</figure>
-	);
+	return <figure id='visual' className='myScroll'></figure>;
 }
 
 export default Visual;
