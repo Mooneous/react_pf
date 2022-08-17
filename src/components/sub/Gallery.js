cb3fc1c90157933aac8ab4d8630648d4779b5a9c
Mooneous <4087022@naver.com>
2022-08-17 16:33:33 +0900

    flickr데이터 가져오기 함수로 묶기

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -4,21 +4,23 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function Gallery() {
-	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 20;
-	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1}`;
-
 	const [Items, setItems] = useState([]);
 	const [Index, setIndex] = useState(0);
 	const [Open, setOpen] = useState(false);
 
-	useEffect(() => {
+	const getFlickr = () => {
+		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 20;
+		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1}`;
+
 		axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
-	}, []);
+	};
+
+	useEffect(getFlickr, []);
 
 	return (
 		<>
