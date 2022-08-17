0d08bfebe267adf1c4e98d4d8c0c5371bafbe1ae
Mooneous <4087022@naver.com>
2022-08-17 09:59:05 +0900

    스크롤 모션 제어 수정

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,8 +1,52 @@
 import Anime from '../../assets/Anime';
+import { useEffect, useState, useRef } from 'react';
 
-function Btns({ setIndex, Scrolled, pos, Index }) {
+function Btns() {
+	const pos = useRef([]);
+	const btnRef = useRef(null);
+	const [Index, setIndex] = useState(0);
+	const [Scrolled, setScrolled] = useState(0);
+
+	const getPos = () => {
+		pos.current = [];
+		const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
+		for (const sec of secs) pos.current.push(sec.offsetTop);
+	};
+
+	const activation = () => {
+		const scroll = window.scrollY;
+		const btns = btnRef.current.children;
+		setScrolled(scroll);
+
+		pos.current.map((pos, idx) => {
+			if (scroll >= pos) {
+				for (const btn of btns) btn.classList.remove('on');
+				btns[idx].classList.add('on');
+			}
+		});
+	};
+
+	useEffect(() => {
+		getPos();
+		window.addEventListener('resize', getPos);
+		window.addEventListener('scroll', activation);
+		return () => {
+			window.removeEventListener('resize', getPos);
+			window.removeEventListener('scroll', activation);
+		};
+	}, []);
+
+	useEffect(() => {
+		console.log(Index);
+		new Anime(window, {
+			prop: 'scroll',
+			value: pos.current[Index],
+			duration: 500,
+		});
+	}, [Index]);
+
 	return (
-		<ul className='scroll_navi'>
+		<ul className='scroll_navi' ref={btnRef}>
 			<li
 				className='on'
 				onClick={() => {
@@ -10,7 +54,7 @@ function Btns({ setIndex, Scrolled, pos, Index }) {
 						if (Scrolled !== pos[0]) {
 							new Anime(window, {
 								prop: 'scroll',
-								value: pos[0],
+								value: pos.current[0],
 								duration: 500,
 							});
 						}
@@ -25,7 +69,7 @@ function Btns({ setIndex, Scrolled, pos, Index }) {
 						if (Scrolled !== pos[1]) {
 							new Anime(window, {
 								prop: 'scroll',
-								value: pos[1],
+								value: pos.current[1],
 								duration: 500,
 							});
 						}
@@ -40,7 +84,7 @@ function Btns({ setIndex, Scrolled, pos, Index }) {
 						if (Scrolled !== pos[2]) {
 							new Anime(window, {
 								prop: 'scroll',
-								value: pos[2],
+								value: pos.current[2],
 								duration: 500,
 							});
 						}
@@ -55,7 +99,7 @@ function Btns({ setIndex, Scrolled, pos, Index }) {
 						if (Scrolled !== pos[3]) {
 							new Anime(window, {
 								prop: 'scroll',
-								value: pos[3],
+								value: pos.current[3],
 								duration: 500,
 							});
 						}
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,75 +5,20 @@ import Pics from './Pics';
 import Vids from './Vids';
 import Btns from './Btns';
 
-import { useRef, useEffect, useState } from 'react';
-import Anime from '../../assets/Anime';
-
 function Main() {
 	//브라우저 리사이즈시 메인 컴포넌트 호출이 되는지안되는지 알아보려고~
 	console.log('main called'); //처음에만 호출되고 리사이즈시 호출안됨
 
-	const main = useRef(null);
-	const pos = useRef([]);
-	const [Index, setIndex] = useState(0);
-	const [Scrolled, setScrolled] = useState(0);
-
-	//섹션의 세로 위치값을 구하는 함수
-	const getPos = () => {
-		pos.current = []; //리사이즈시마다 위치값(pos) 추가되고있으므로 빈배열 만들어 초기화한 다음 담기
-		const secs = main.current.querySelectorAll('.myScroll');
-		//반복문(메인페이지섹션높이값)
-		for (const sec of secs) pos.current.push(sec.offsetTop);
-		console.log(pos.current);
-	};
-
-	//스크롤 위치에 따라서 버튼 활성화 함수
-	const activation = () => {
-		const scroll = window.scrollY;
-		const btns = main.current.querySelectorAll('.scroll_navi li');
-		setScrolled(scroll);
-
-		//pos.current에 등록된 각 섹션의 세로 위치값을 반복
-		pos.current.map((pos, idx) => {
-			//현재 스크롤된 거리값이 각 섹션의 위치값보다 같거나 크면
-			//기존 버튼 모두 비활성화시키고 해당 순번의 버튼만 활성화
-			if (scroll >= pos) {
-				for (const btn of btns) btn.classList.remove('on');
-				btns[idx].classList.add('on');
-			}
-		});
-	};
-
-	useEffect(() => {
-		getPos(); //마운트될때
-		window.addEventListener('resize', getPos);
-		window.addEventListener('scroll', activation);
-
-		return () => {
-			//언마운트될때
-			window.removeEventListener('resize', getPos);
-			window.removeEventListener('scroll', activation);
-		};
-	}, []);
-
-	useEffect(() => {
-		console.log(Index);
-		new Anime(window, {
-			prop: 'scroll',
-			value: pos.current[Index],
-			duration: 500,
-		});
-	}, [Index]);
-
 	return (
-		<main ref={main}>
+		<>
 			<Header type={'main'} />
 			<Visual />
 			<News />
 			<Pics />
 			<Vids />
-			<Btns Index={Index} setIndex={setIndex} Scrolled={Scrolled} pos={pos.current} />
+			<Btns />
 			{/*setIndex라는 state함수를 setIndex라는 props 이름으로 전달*/}
-		</main>
+		</>
 	);
 }
 
