a0917ca8c583576ef533b867db3b92c7d8036dde
Mooneous <4087022@naver.com>
2022-08-17 11:12:34 +0900

    기본 스와이퍼 연결

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,7 +1,15 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 function Vids() {
 	return (
 		<section id='vids' className='myScroll'>
-			<h1>Recent Youtube</h1>
+			<Swiper>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+			</Swiper>
 		</section>
 	);
 }
