8996d98e14584f605c2ae5fcfe0db19e0e37e07e
Mooneous <4087022@naver.com>
2022-08-17 16:38:50 +0900

    async await로 코드 동기화 시키고 이후 frame요소 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,23 +1,25 @@
 import Layout from '../common/Layout';
 import Pop from '../common/Pop';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function Gallery() {
+	const frame = useRef(null);
 	const [Items, setItems] = useState([]);
 	const [Index, setIndex] = useState(0);
 	const [Open, setOpen] = useState(false);
 
-	const getFlickr = () => {
+	const getFlickr = async () => {
 		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
 		const method_interest = 'flickr.interestingness.getList';
-		const num = 20;
+		const num = 500;
 		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1}`;
 
-		axios.get(url).then((json) => {
+		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
+		frame.current.classList.add('on');
 	};
 
 	useEffect(getFlickr, []);
@@ -25,7 +27,7 @@ function Gallery() {
 	return (
 		<>
 			<Layout name={'Gallery'}>
-				<div className='frame on'>
+				<div className='frame' ref={frame}>
 					{Items.map((pic, idx) => {
 						return (
 							<article
