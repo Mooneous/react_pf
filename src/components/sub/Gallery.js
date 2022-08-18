cc6b2ebfcafe3f87941593ae1a02a997248f44e6
Mooneous <4087022@naver.com>
2022-08-18 10:38:22 +0900

    검색어 미입력시 경고창 추가 및 검색요청후 인풋창 초기화

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -7,6 +7,7 @@ import { useEffect, useState, useRef } from 'react';
 
 function Gallery() {
 	const frame = useRef(null);
+	const input = useRef(null);
 	const [Items, setItems] = useState([]);
 	const [Index, setIndex] = useState(0);
 	const [Open, setOpen] = useState(false);
@@ -14,7 +15,7 @@ function Gallery() {
 	const [EnableClick, setEnableClick] = useState(false);
 	//masonry 전환속도 옵션객체 설정
 	const masonryOptions = { transitionDuration: '0.5s' };
-	const num = 500;
+	const num = 50;
 	const user = '164021883@N04';
 
 	const getFlickr = async (opt) => {
@@ -74,6 +75,23 @@ function Gallery() {
 					}}>
 					Interest Gallery
 				</button>
+
+				<div className='searchBox'>
+					<input type='text' ref={input} />
+					<button
+						onClick={() => {
+							const result = input.current.value.trim();
+							if (!result) return alert('검색어를 입력하세요');
+							if (!EnableClick) return;
+							setEnableClick(false);
+							setLoading(true);
+							frame.current.classList.remove('on');
+							getFlickr({ type: 'search', tag: result });
+							input.current.value = '';
+						}}>
+						search
+					</button>
+				</div>
 
 				{Loading && <img className='loading' src={process.env.PUBLIC_URL + '/img/loading.gif'} />}
 
