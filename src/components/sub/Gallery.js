0d134f81c66c727f2d04dbbe4e2fa3d0b764829d
Mooneous <4087022@naver.com>
2022-08-18 11:27:24 +0900

    프로필 정보 출력, 해당 이미지가 없을시 대체이미지 출력 그리고 스타일링

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -25,14 +25,15 @@ function Gallery() {
 		const method_search = 'flickr.photos.search';
 		let url = '';
 		//객체로 전달되는 type에 따라 호출한 URL을 새로 만들고 axios에 전달
-		if (opt.type === 'interest')
+		if (opt.type === 'interest') {
 			url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1}`;
-		if (opt.type === 'user')
+		}
+		if (opt.type === 'user') {
 			url = `https://www.flickr.com/services/rest/?method=${method_user}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&user_id=${user}`;
-		if (opt.type === 'search')
+		}
+		if (opt.type === 'search') {
 			url = `https://www.flickr.com/services/rest/?method=${method_search}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&tags=${opt.tag}`;
-
-		const url_user = `https://www.flickr.com/services/rest/?method=${method_user}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&user_id=${user}`;
+		}
 
 		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
@@ -119,6 +120,20 @@ function Gallery() {
 											/>
 										</div>
 										<h2>{pic.title}</h2>
+
+										<div className='profile'>
+											<img
+												src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+												alt={pic.owner}
+												onError={(e) => {
+													e.target.setAttribute(
+														'src',
+														'https://www.flickr.com/images/buddyicon.gif'
+													);
+												}}
+											/>
+											<span>{pic.owner}</span>
+										</div>
 									</div>
 								</article>
 							);
