b6b17d6055282d9af90bc09df91ca2b001ef88b7
Mooneous <4087022@naver.com>
2022-08-19 12:58:46 +0900

    useDispatch로 내부 컴포넌트에서 변경한 데이터를 액션객체에 담아서 리듀서에 전달후 store전역 데이터 변경

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,12 +1,29 @@
 import Layout from '../common/Layout';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setMembers } from '../../redux/action';
 
 function Department() {
+	//해당 컴포넌트에서 만들어질 액션 객체를 리듀서에 전달해주는 dispath함수 활성화
+	const dispatch = useDispatch();
 	const path = process.env.PUBLIC_URL;
 	const Members = useSelector((store) => store.memberReducer.members);
 
 	return (
 		<Layout name={'Department'}>
+			<button
+				onClick={() => {
+					//불변성 유지를 위해서 기존 데이터를 deep copy후
+					//copy된 데이터를 변경
+					const newMembers = [...Members];
+					newMembers[0].name = 'Elizabeth';
+					//변경된 데이터를 action.js에서 가져온 액션생성함수로 액션객체 반환받고
+					//반환받은 액션객체를 바로 dispatch함수의 인수로 넣어서 리듀서에 전달
+					dispatch(setMembers(newMembers));
+				}}>
+				Member Change
+			</button>
+			<br />
+
 			{Members.map((member, idx) => (
 				<article key={idx}>
 					<div className='inner'>
@@ -23,12 +40,25 @@ function Department() {
 					</div>
 				</article>
 			))}
-			<p>Department</p>
 		</Layout>
 	);
 }
 
 export default Department;
+
+{
+	/* 
+<article>
+	<div className="inner">
+		<div className="pic">
+			<img src="" alt="" />
+		</div>
+		<h2>멤버 이름</h2>
+		<p>맴버 직책</p>
+	</div>
+</article> 
+*/
+}
 
 /*<article>
 	<div className='inner'>
