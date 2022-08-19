b841e81c7536e09bd52254c9cdcbef2196ed1fba
Mooneous <4087022@naver.com>
2022-08-19 11:35:08 +0900

    리듀서 생성

---
+++ src/redux/reducer.js
@@ -0,0 +1,53 @@
+import { combineReducers } from 'redux';
+
+const initMember = {
+	members: [
+		{
+			name: 'Julia',
+			position: 'President',
+			pic: 'member1.jpg',
+		},
+		{
+			name: 'David',
+			position: 'Vice President',
+			pic: 'member2.jpg',
+		},
+		{
+			name: 'Emily',
+			position: 'UI Designer',
+			pic: 'member3.jpg',
+		},
+		{
+			name: 'Paul',
+			position: 'Front-end Engineer',
+			pic: 'member4.jpg',
+		},
+		{
+			name: 'Sara',
+			position: 'Back-end Engineer',
+			pic: 'member5.jpg',
+		},
+		{
+			name: 'Michael',
+			position: 'Project Manager',
+			pic: 'member6.jpg',
+		},
+	],
+};
+
+//초기데이터를 state에 저장했다가 추후 action객체가 전달되면
+//action객체의 타입에 따라 기존 데이터를 변경해서 리턴
+const memberReducer = (state = initMember, action) => {
+	switch (action.type) {
+		case 'SET_MEMBERS':
+			return { ...state, members: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+//전달된 각각의 reducer를 하나로 합쳐서 반환
+const reducers = combineReducers({ memberReducer });
+
+export default reducers;
