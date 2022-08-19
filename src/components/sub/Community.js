c358e378df01d298af2722e7a6c0aa61fb7a930c
Mooneous <4087022@naver.com>
2022-08-19 14:01:23 +0900

    ...커뮤니티오류잡기

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -12,8 +12,8 @@ function Community() {
 		return JSON.parse(data);
 	};
 
-	//초기 Posts 스테이트에 로컬스토리지의 데이터를 가져와서 저장
-	const [Posts, setPosts] = useState([getLocalData()]);
+	//초기 Posts스테이트에 로컬스토리지의 데이터를 가져와서 저장
+	const [Posts, setPosts] = useState(getLocalData());
 	const [Allowed, setAllowed] = useState(true);
 
 	//기존 폼요소 초기화 함수
@@ -33,11 +33,7 @@ function Community() {
 
 	//글삭제 함수
 	const deletePost = (index) => {
-		//기존 Posts스테이트의 배열값을 filter로 반복돌면서 현재 반복도는 순번값과 ,index파라미터로 전달된 삭제할 순번이 같지 않은 글만 반환
-		const newPosts = Posts.filter((_, idx) => idx !== index); //index : 삭제할 순번
-		//console.log(newPosts);
-		//삭제순번의 글이 제외되어 반환된 데이터로 다시 state변경
-		//해당 setPosts시 전개연산자(..블라블라)를 쓰지 않는 이유는 filter메서드 자체가 새로운배열을 이미 deepCopy해서 반환하기 때문
+		const newPosts = Posts.filter((_, idx) => idx !== index);
 		setPosts(newPosts);
 	};
 
@@ -84,16 +80,15 @@ function Community() {
 		);
 	};
 
-	//Posts값이 변경될떄마다 로컬 스토리지에 기존 데이터를 다시 문자열로 변환해서 저장
+	//Posts값이 변경될때마다 로컬 스토리지에 기존 데이터를 다시 문자열로 변환해서 저장
 	useEffect(() => {
 		localStorage.setItem('post', JSON.stringify(Posts));
-		//console.log(Posts);
 	}, [Posts]);
 
 	return (
 		<Layout name={'Community'}>
 			<div className='inputBox'>
-				<input type='text' placeholdler='제목을 입력하세요' ref={input} />
+				<input type='text' placeholder='제목을 입력하세요' ref={input} />
 				<br />
 				<textarea cols='30' rows='3' placeholder='본문을 입력하세요' ref={textarea}></textarea>
 				<br />
@@ -104,7 +99,6 @@ function Community() {
 			</div>
 
 			<div className='showBox'>
-				{/* 배열에 객체값이 추가가되면 반복을 돌면서 리턴문으로 목록 출력 */}
 				{Posts.map((post, idx) => {
 					return (
 						<article key={idx}>
@@ -138,7 +132,6 @@ function Community() {
 
 									<div className='btnSet'>
 										<button onClick={() => enableUpdate(idx)}>EDIT</button>
-										{/* 각 게시글 목록을 생성할때 삭제버튼까지 같이 생성, 삭제버튼 클릭시 삭제하려고 하는 해당 순번을 인수로 전달 */}
 										<button onClick={() => deletePost(idx)}>DELETE</button>
 									</div>
 								</>
@@ -152,10 +145,3 @@ function Community() {
 }
 
 export default Community;
-
-/*
-Create : 데이터베이스에 정보 생성 (데이터 저장)
-Read : 데이터베이스에 정보 불러 (데이터 출력)
-Update : 데이터베이스에 정보 업데이트 (데이터 수정)
-Delete : 데이터베이스에 정보 삭제 (데이터 제거)
-*/
