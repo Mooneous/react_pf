dfd347aaa33a277fa909a1e34e4acf7345e4eff4
Mooneous <4087022@naver.com>
2022-08-19 15:43:06 +0900

    유튜브 미리보기 팝업 연결

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -3,37 +3,55 @@ import { Pagination, Navigation, Autoplay } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import Pop from '../common/Pop';
 
 function Vids() {
+	const pop = useRef(null);
+	const [Index, setIndex] = useState(0);
 	const { youtube } = useSelector((store) => store.youtubeReducer);
 	return (
-		<section id='vids' className='myScroll'>
-			<Swiper
-				pagination={{ clickable: true }}
-				navigation={true}
-				modules={[Pagination, Navigation, Autoplay]}
-				autoplay={{ delay: 2000, disableOnInteraction: true }}
-				spaceBetween={50}
-				loop={true}
-				slidesPerView={3}
-				centeredSlides={true}>
-				{/*	centeredSlides={true} : 가운데요소 활성화*/}
-				{youtube.map((vid, idx) => {
-					if (idx >= 4) return;
-					return (
-						<SwiperSlide key={vid.id}>
-							<div className='inner'>
-								<div className='pic'>
-									<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+		<>
+			<section id='vids' className='myScroll'>
+				<Swiper
+					modules={[Pagination, Navigation, Autoplay]}
+					autoplay={{ delay: 2000, disableOnInteraction: true }}
+					pagination={{ clickable: true }}
+					navigation={true}
+					spaceBetween={50}
+					loop={true}
+					slidesPerView={3}
+					centeredSlides={true}>
+					{youtube.map((vid, idx) => {
+						if (idx >= 4) return;
+						return (
+							<SwiperSlide key={vid.id}>
+								<div className='inner'>
+									<div
+										className='pic'
+										onClick={() => {
+											setIndex(idx);
+											pop.current.open();
+										}}>
+										<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+									</div>
+									<h2>{vid.snippet.title}</h2>
 								</div>
-								<h2>{vid.snippet.title}</h2>
-							</div>
-						</SwiperSlide>
-					);
-				})}
-			</Swiper>
-		</section>
+							</SwiperSlide>
+						);
+					})}
+				</Swiper>
+			</section>
+
+			<Pop ref={pop}>
+				{youtube.length !== 0 && (
+					<iframe
+						src={`https://www.youtube.com/embed/${youtube[Index].snippet.resourceId.videoId}`}
+						frameBorder='0'></iframe>
+				)}
+			</Pop>
+		</>
 	);
 }
 
