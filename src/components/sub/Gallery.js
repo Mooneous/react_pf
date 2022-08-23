cce054c65eafeda84de521d7aa4a2300ccdb9a0c
Mooneous <4087022@naver.com>
2022-08-23 10:39:00 +0900

    gallery.js 슬라이스 설정

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -4,7 +4,7 @@ import Pop from '../common/Pop';
 import Masonry from 'react-masonry-component';
 import { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import * as types from '../../redux/actionType';
+import { fetchFlickr } from '../../redux/flickrSlice';
 
 function Gallery() {
 	const dispatch = useDispatch();
@@ -68,7 +68,7 @@ function Gallery() {
 
 	//Opt값이 변경될떄마다 dispath로 변경된 해당 Opt값을 Flickr_start액션객체에 담아서 saga에 전달
 	useEffect(() => {
-		dispatch({ type: types.FLICKR.start, Opt });
+		dispatch(fetchFlickr(Opt));
 	}, [Opt]);
 
 	//flickr데이터가 변경될때마다 (새로운데이터 요청을 해서 해당 요청이 완료될때마다) 로딩제거함수 호출
